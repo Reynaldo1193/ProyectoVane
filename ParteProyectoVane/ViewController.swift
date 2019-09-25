3f9090a7982f7742bb9f490c3bb0a20e26ae3f3d
joan-daniel <joandaniel18@gmail.com>
2019-09-25 18:01:52 -0500

    Fin falta ant

--- ParteProyectoVane/ViewController.swift
+++ ParteProyectoVane/ViewController.swift
@@ -36,26 +36,53 @@ class ViewController: UIViewController {
             print("entra")
             img.image = UIImage(named: "Perro1")
             
-            speechUtterance.speechString = "Había una vez un perro"
+            let speechUtterance : AVSpeechUtterance = AVSpeechUtterance(string: "Había una vez un perro")
+            speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate/2
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
             speechSynthesizer.speak(speechUtterance)
+            
             break
         case 2:
             print("entra")
             img.image = UIImage(named: "Perro2")
+            
+            let speechUtterance : AVSpeechUtterance = AVSpeechUtterance(string: "Que se llamaba pegamento")
+            speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate/2
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
+            speechSynthesizer.speak(speechUtterance)
+            
             break
 
         case 3:
             print("entra")
             img.image = UIImage(named: "Perro3")
+            
+            let speechUtterance : AVSpeechUtterance = AVSpeechUtterance(string: "Se calló")
+            speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate/2
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
+            speechSynthesizer.speak(speechUtterance)
+            
             break
 
         case 4:
             print("entra")
             img.image = UIImage(named: "Perro4")
+            
+            let speechUtterance : AVSpeechUtterance = AVSpeechUtterance(string: "Y se pegó")
+            speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate/2
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
+            speechSynthesizer.speak(speechUtterance)
+            
             break
         case 5:
             print("entra")
             img.image = UIImage(named: "Perro5")
+            
+            let speechUtterance : AVSpeechUtterance = AVSpeechUtterance(string: "Fin")
+            speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate/2
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
+            speechSynthesizer.speak(speechUtterance)
+            
             break
 
 
